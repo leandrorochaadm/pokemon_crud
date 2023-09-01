cb93a2f121e15dbd4741126a744b617e93fb9875
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-09-01 10:12:41 -0300

    DetailsEntity

---
+++ lib/app/domain/enties/details_entity.dart
@@ -0,0 +1,27 @@
+class DetailsEntity {
+  List<String> abilities;
+  int baseExperience;
+  List<String> forms;
+  int height;
+  int id;
+  List<String> moves;
+  String name;
+  int order;
+  List<String> types;
+  int weight;
+  String species;
+
+  DetailsEntity({
+    required this.abilities,
+    required this.baseExperience,
+    required this.forms,
+    required this.height,
+    required this.id,
+    required this.moves,
+    required this.name,
+    required this.order,
+    required this.species,
+    required this.types,
+    required this.weight,
+  });
+}
--- lib/app/domain/enties/enties.dart
+++ lib/app/domain/enties/enties.dart
@@ -1,4 +1,2 @@
 export 'details_entity.dart';
 export 'pokemon_entity.dart';
-export 'type_entity.dart';
-export 'types_entity.dart';
