81b054d3460066f5b3fb1d9c51d241d9ad6322f4
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-09-01 11:44:05 -0300

    ItemWidget

---
+++ lib/app/ui/pages/details/widgets/item_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ItemWidget extends StatelessWidget {
+  final String label;
+  final String? description;
+
+  const ItemWidget({
+    super.key,
+    required this.label,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(label,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+        const SizedBox(width: 12),
+        Text(
+          description ?? '',
+          style: const TextStyle(fontSize: 16),
+        ),
+      ],
+    );
+  }
+}
